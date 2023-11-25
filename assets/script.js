258f239b2b8573c14f2f0d5b2f26cd1a1f465eb9
HugEau <hugo260704chesnel@gmail.com>
2023-11-25 14:19:15 +0100

    Added comments

--- assets/script.js
+++ assets/script.js
@@ -17,23 +17,31 @@ const slides = [
 	}
 ]
 
+// Initialisation compteur de slide
 let currentSlide = 0;
+
+//  Récupération id des dots, de la bannière, du texte et des flèches
 let currentDotSlide = document.getElementById("dot0");
 let imageSlide = document.getElementById("bannerImg");
 let txtSlide = document.getElementById("pTxt");
 let leftArrow = document.getElementById("arrowLeft");
 let rightArrow = document.getElementById("arrowRight");
 
+// Fonction de MàJ de la slide (index => n° de la slide à initialiser)
 function updateSlide(index) {
+    // Modification du dot sélectionné
     currentDotSlide.classList.remove("dot_selected");
     currentDotSlide = document.getElementById("dot" + index);
     currentDotSlide.classList.add("dot_selected");
 
+    // Modification image et texte
     imageSlide.src = "./assets/images/slideshow/" + slides[index].image;
     txtSlide.innerHTML = slides[index].tagLine;
 }
 
+// Fontion pour Slide à gauche
 function slideLeft() {
+    // Contrôle du n° de slide pour définir l'action à faire
     if (currentSlide > 0) {
         currentSlide--;
     } else {
@@ -42,7 +50,9 @@ function slideLeft() {
     updateSlide(currentSlide);
 }
 
+// Fontion pour Slide à droite
 function slideRight() {
+    // Contrôle du n° de slide pour définir l'action à faire
     if (currentSlide < slides.length - 1) {
         currentSlide++;
     } else {
@@ -51,5 +61,7 @@ function slideRight() {
     updateSlide(currentSlide);
 }
 
+
+// Event Listener sur les flècles pour Slide
 leftArrow.addEventListener("click", slideLeft);
 rightArrow.addEventListener("click", slideRight);
